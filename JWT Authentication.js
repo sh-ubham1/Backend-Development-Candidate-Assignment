bc5d160d3137a587519e0d3ccc5bacec3c0dac13
Shubham Agrahari <62196739+sh-ubham1@users.noreply.github.com>
2024-10-11 22:44:05 +0530

    Update JWT Authentication.js

--- JWT Authentication.js
+++ JWT Authentication.js
@@ -1,1 +1,16 @@
+const bcrypt = require('bcryptjs');
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  
+ 
+  const user = { username: 'user1', password: '$2a$10$...' }; 
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send('Invalid credentials');
+  }
+
+  const token = jwt.sign({ username: user.username }, secretKey, { expiresIn: '1h' });
+  res.json({ token });
+});
 
